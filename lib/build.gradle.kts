d545ecb410b73f9c4bef98c0f823cc81e10c260c
Ivan Plantevin <ivan@strmprivacy.io>
2023-05-03 14:26:53 +0200

    fix(strm-2387): use api instead of implementation dependency for sl4j (#16)
    Fixes #10

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -14,9 +14,9 @@ plugins {
 dependencies {
     implementation("io.strmprivacy.schemas:schema-common:2.0.1")
     implementation("org.apache.avro:avro:1.11.0")
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 
+    api("org.slf4j:slf4j-api:$slf4jVersion")
     api("org.eclipse.jetty:jetty-client:$jettyVersion")
     api("org.eclipse.jetty.http2:http2-client:$jettyVersion")
     api("org.eclipse.jetty.http2:http2-http-client-transport:$jettyVersion")
